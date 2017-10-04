830d26fb2a80bdea88fc81320121fb701785dc72
astidwiyana15 <dwiyanasti@gmail.com>
2017-10-04 13:32:55 +0700

    latihannya blm

--- src/main/java/com/example/dao/StudentMapper.java
+++ src/main/java/com/example/dao/StudentMapper.java
@@ -4,17 +4,29 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Many;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
+import com.example.model.CourseModel;
 import com.example.model.StudentModel;
 
 @Mapper
 public interface StudentMapper
 {
     @Select("select npm, name, gpa from student where npm = #{npm}")
+    @Results(value = {
+    		@Result(property="npm", column="npm"),
+    		@Result(property="name",column="name"),
+    		@Result(property="gpa", column="gpa"),
+    		@Result(property="courses",column="npm",
+    			javaType = List.class,
+    			many=@Many(select="selectCourses"))
+    })
     StudentModel selectStudent (@Param("npm") String npm);
 
     @Select("select npm, name, gpa from student")
@@ -29,5 +41,10 @@ public interface StudentMapper
     @Update("UPDATE student SET name = #{name}, gpa = #{gpa} WHERE npm = #{npm}")
     void updateStudent(StudentModel student);
     
+    @Select("select course.id_course, name, credits " + 
+            "from studentcourse join course "+
+    		"on studentcourse.id_course = course.id_course " +
+            "where studentcourse.npm = #{npm}")
+    List<CourseModel> selectCourses (@Param("npm")String npm);
     
 }
